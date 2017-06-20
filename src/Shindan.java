3d3092ec360b286765f4289f7d6aec18a4101d90
Ayumu Kamimega <ayumu.kamimega.sys@gmail.com>
2017-06-20 11:19:08 +0900

    first commit

--- src/Shindan.java
+++ src/Shindan.java
@@ -1,16 +1,39 @@
+import java.util.Scanner;
+
 public class Shindan {
 	
 	public static void main(String[] args) {
 		//メイン処理
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
-	
 	
 	public static int situmon1() {
 		int ans1 = 0;
 		//質問内容
 		//回答内容（1 : 男性 , 2 : 女性）
 		//ユーザーが答えを入力
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
@@ -19,30 +42,72 @@ public class Shindan {
 	
 	public static int situmon2() {
 		int ans2 = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
 		return ans2;
 	}
 	
-	public int situmon3() {
+	public static int situmon3() {
 		int ans3 = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
 		return ans3;
 	}
 	
-	public int situmon4() {
+	public static int situmon4() {
 		int ans4 = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
 		return ans4;
 	}
 	
-	public int situmon5() {
+	public static int situmon5() {
 		int ans5 = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
