ee5972beeee31de4f7d829c19c27278c37df5e74
Ayumu Kamimega <ayumu.kamimega.sys@gmail.com>
2017-06-20 10:18:55 +0900

    first commit

---
+++ src/Shindan.java
@@ -0,0 +1,52 @@
+public class Shindan {
+	
+	public static void main(String[] args) {
+		//メイン処理
+		
+	}
+	
+	
+	public static int situmon1() {
+		int ans1 = 0;
+		//質問内容
+		//回答内容（1 : 男性 , 2 : 女性）
+		//ユーザーが答えを入力
+		
+		
+		
+		return ans1;
+	}
+	
+	public static int situmon2() {
+		int ans2 = 0;
+		
+		
+		
+		return ans2;
+	}
+	
+	public int situmon3() {
+		int ans3 = 0;
+		
+		
+		
+		return ans3;
+	}
+	
+	public int situmon4() {
+		int ans4 = 0;
+		
+		
+		
+		return ans4;
+	}
+	
+	public int situmon5() {
+		int ans5 = 0;
+		
+		
+		
+		return ans5;
+	}
+	
+}
